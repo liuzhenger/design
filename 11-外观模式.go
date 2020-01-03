565c7c6d7761bb66208503a687fe0b75b1ebd356
LiuZheng <liuzhengr@qq.com>
2020-01-03 16:58:22 +0700

    update

--- 11-外观模式.go
+++ 11-外观模式.go
@@ -1,1 +1,32 @@
 package design
+
+import "fmt"
+
+type WeaponSystem struct {
+}
+
+func (w *WeaponSystem) Bullet() {
+	fmt.Println("上子弹")
+}
+
+func (w *WeaponSystem) Fire() {
+	fmt.Println("开火")
+}
+
+type UserSystem struct {
+}
+
+func (u *UserSystem) Score() {
+	fmt.Println("得分")
+}
+
+type Facade struct {
+	*WeaponSystem
+	*UserSystem
+}
+
+func (f *Facade) Shooting() {
+	f.Bullet()
+	f.Fire()
+	f.Score()
+}
--- 11_test.go
+++ 11_test.go
@@ -1,1 +1,16 @@
 package design
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_11_1(t *testing.T) {
+	client := new(Facade)
+	client.WeaponSystem = new(WeaponSystem)
+	client.UserSystem = new(UserSystem)
+	client.Fire()
+	client.Bullet()
+	fmt.Println("-----------")
+	client.Shooting()
+}
